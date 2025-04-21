e2ef22f98d7ae6ef3b1e8ca1b913da1dc4f0faf1
The_NorthernFox Aka Erik Ruiz <erikruiz13@gmail.com>
2025-04-21 17:48:20 +0000

    Proyecto Actualizado

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './components/Home.jsx';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
